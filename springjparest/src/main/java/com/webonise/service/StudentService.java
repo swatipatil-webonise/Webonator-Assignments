bb82e60e69a87d9787e5d9c29883bae821fc8b79
swatipatil-webonise <55143985+swatipatil-webonise@users.noreply.github.com>
2019-10-09 14:48:54 +0530

    Update StudentService.java

--- springjparest/src/main/java/com/webonise/service/StudentService.java
+++ springjparest/src/main/java/com/webonise/service/StudentService.java
@@ -2,9 +2,11 @@ package com.webonise.service;
 
 import com.webonise.model.Student;
 import com.webonise.repository.StudentDao;
+import jdk.internal.instrumentation.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.logging.LogManager;
 
 @Service
 public class StudentService {
@@ -12,31 +14,57 @@ public class StudentService {
     @Autowired
     private StudentDao studentDao;
 
+    private Logger logger = (Logger) LogManager.getLogManager().getLogger("logger");
+
     public List<Student> getAllStudents() {
-        return studentDao.findAll();
+        try {
+            return studentDao.findAll();
+        } catch(Exception e) {
+            logger.error("Exception occurred at getAllStudents() in StudentService.class.", e);
+        }
+        return null;
     }
 
-    public Student createOrSaveStudent(Student student) {
-        return studentDao.save(student);
+    public Student saveStudent(Student student) {
+        try {
+            return studentDao.save(student);
+        } catch(Exception e) {
+            logger.error("Exception occurred at saveStudent() in StudentService.class.", e);
+        }
+        return null;
     }
 
     public Student getStudentByID(int id) {
-        return studentDao.findById(id).get();
+        try {
+            return studentDao.findById(id).get();
+        } catch(Exception e) {
+            logger.error("Exception occurred at getStudentById() in StudentService.class.", e);
+        }
+        return null;
     }
 
-    public Student getStudent(Student newStudent, int id) {
-        return studentDao.findById(id).map(student -> {
-            student.setId(newStudent.getId());
-            student.setAge(newStudent.getAge());
-            student.setName(newStudent.getName());
-            return studentDao.save(student);
-        }).orElseGet(() -> {
-            newStudent.setId(id);
-            return studentDao.save(newStudent);
-        });
+    public Student updateStudent(Student newStudent) {
+        try {
+            return studentDao.findById(newStudent.getId()).map(student -> {
+                student.setId(newStudent.getId());
+                student.setAge(newStudent.getAge());
+                student.setName(newStudent.getName());
+                return studentDao.save(student);
+            }).orElseGet(() -> {
+                newStudent.setId(newStudent.getId());
+                return studentDao.save(newStudent);
+            });
+        } catch(Exception e) {
+            logger.error("Exception occurred at getStudentById() in StudentService.class.", e);
+        }
+        return null;
     }
 
     public void deleteStudent(int id) {
-        studentDao.deleteById(id);
+        try {
+            studentDao.deleteById(id);
+        } catch(Exception e) {
+            logger.error("Exception occurred at getStudentById() in StudentService.class.", e);
+        }
     }
 }
