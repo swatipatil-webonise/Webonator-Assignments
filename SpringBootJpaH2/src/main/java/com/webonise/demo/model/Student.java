0419b7c5a80061dc8949b860008a3149e33815a6
swatipatil-webonise <55143985+swatipatil-webonise@users.noreply.github.com>
2019-10-25 15:29:44 +0530

    add student file with lombok library

--- SpringBootJpaH2/src/main/java/com/webonise/demo/model/Student.java
+++ SpringBootJpaH2/src/main/java/com/webonise/demo/model/Student.java
@@ -1,8 +1,5 @@
 package com.webonise.demo.model;
-<<<<<<< HEAD
 
-=======
->>>>>>> 567948f4b1a592d602cdb6bbd4177a4cd82a14c3
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,45 +8,14 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-<<<<<<< HEAD
 @Getter
 @Setter
 @ToString
-public class Student implements Serializable{
-=======
 public class Student implements Serializable {
->>>>>>> 567948f4b1a592d602cdb6bbd4177a4cd82a14c3
 
 	private static final long serialVersionUID = 1L;
 	@Id
 	private int id;
 	private String name;
 	private int age;
-<<<<<<< HEAD
-=======
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
->>>>>>> 567948f4b1a592d602cdb6bbd4177a4cd82a14c3
 }
