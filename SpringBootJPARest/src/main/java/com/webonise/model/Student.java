473f854fa8f238c7b11800c10693e81f2e06f623
swatipatil-webonise <55143985+swatipatil-webonise@users.noreply.github.com>
2019-10-25 15:11:29 +0530

    add student with serailUID

--- SpringBootJPARest/src/main/java/com/webonise/model/Student.java
+++ SpringBootJPARest/src/main/java/com/webonise/model/Student.java
@@ -1,9 +1,8 @@
 package com.webonise.model;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,11 +11,11 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Student {
-	
+public class Student implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	@Id
 	private int id;
 	private String name;
 	private int age;
-	
 }
