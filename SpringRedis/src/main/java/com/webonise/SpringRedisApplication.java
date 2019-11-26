3955e8a14cc6539f905d44bdd3f599c467f51d73
swati <swati.patil@weboniselab.com>
2019-11-26 19:25:16 +0530

    remove scheduling conguration from properties file

--- SpringRedis/src/main/java/com/webonise/SpringRedisApplication.java
+++ SpringRedis/src/main/java/com/webonise/SpringRedisApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import com.webonise.model.User;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringRedisApplication {
 
 	@Bean
