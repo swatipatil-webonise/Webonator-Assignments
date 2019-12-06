27172d98497ad6839ab3ee806a6a4bcbbb0a79d8
swati <swati.patil@weboniselab.com>
2019-12-06 18:49:07 +0530

    add graphql example in springboot

---
+++ Springboot-GraphQl/src/main/java/com/webonise/graphql/exception/NotFoundException.java
@@ -0,0 +1,39 @@
+package com.webonise.graphql.exception;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+public class NotFoundException extends RuntimeException implements GraphQLError {
+	
+	private static final long serialVersionUID = 1L;
+	private final int errorCode;
+	private final String errorMessage;
+	
+	public NotFoundException(int errorCode, String errorMessage) {
+		super(errorMessage);
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+	
+	@Override
+	public Map<String, Object> getExtensions(){
+		Map<String, Object> customAttributes = new LinkedHashMap<String, Object>();
+		customAttributes.put("errorCode", this.errorCode);
+		customAttributes.put("errorMessage", this.errorMessage);
+		return customAttributes;
+	}
+
+	@Override
+	public List<SourceLocation> getLocations() {
+		return null;
+	}
+
+	@Override
+	public ErrorType getErrorType() {
+		return null;
+	}
+}
